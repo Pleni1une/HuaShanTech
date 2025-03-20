e67101b56e540685e6782057a111286f64e4cff7
Z_Yuan <Z_Yuan_@outlook.com>
2025-03-20 18:46:21 +0800

    new file:   main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 400,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    mainWindow.loadFile(path.join(__dirname, 'src/Pages/Login.html'))
+
+    // 开发模式下打开开发者工具
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools()
+    }
+    
+    mainWindow.setMenu(null)
+
+    icon: path.join(__dirname, './src/Resources/icon.ico')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
